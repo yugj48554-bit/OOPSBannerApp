1beaf0302220fe63e9dd7b7aefcf0d2879c185a3
yugj48554-bit <yugj48554@gmail.com>
2026-03-03 15:19:48 +0530

    UC1

--- oopsbannerapp.java
+++ oopsbannerapp.java
@@ -1,21 +1,5 @@
-public class oopsbannerapp {
-
-    // Main method to run the banner display
-    public static void main(String[] args) {
-
-        // Line 1
-        System.out.println("  ***  " + " " + "  ***  " + " " + "****** " + " " + " ***** ");
-        // Line 2
-        System.out.println(" **  ** " + " " + " **  ** " + " " + "**   ** " + " " + "**     ");
-        // Line 3
-        System.out.println("**    **" + " " + "**    **" + " " + "**   ** " + " " + "**     ");
-        // Line 4
-        System.out.println("**    **" + " " + "**    **" + " " + "****** " + " " + " ***** ");
-        // Line 5
-        System.out.println("**    **" + " " + "**    **" + " " + "**     " + " " + "     **");
-        // Line 6
-        System.out.println(" **  ** " + " " + " **  ** " + " " + "**     " + " " + "     **");
-        // Line 7
-        System.out.println("  ***  " + " " + "  ***  " + " " + "**     " + " " + " ***** ");
+public class oopsbannerapp{
+    public static void main(String[]args){
+        System.out.println("OOPS");
     }
 }

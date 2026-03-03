4bb4fc6cbcb9f0f2788b337360e7138f53c03b99
yugj48554-bit <yugj48554@gmail.com>
2026-03-03 15:29:59 +0530

    UC4

--- oopsbannerapp.java
+++ oopsbannerapp.java
@@ -1,12 +1,21 @@
 public class oopsbannerapp{
      public static void main(String[] args) {
 
-        System.out.println(String.join(" ", "  *  ", "  *  ", "** ", " *** "));
-        System.out.println(String.join(" ", " *  *", " *  *", "*   *", "**     "));
-        System.out.println(String.join(" ", "*    *", "*    *", "*   *", "**     "));
-        System.out.println(String.join(" ", "*    *", "*    *", "** ", " *** "));
-        System.out.println(String.join(" ", "*    *", "*    *", "*     ", "     *"));
-        System.out.println(String.join(" ", " *  *", " *  *", "*     ", "     *"));
-        System.out.println(String.join(" ", "  *  ", "  *  ", "*     ", " ** "));
+        // Define String Array to hold the OOPS banner lines
+        String[] lines = new String[7];
+
+        // Populate each line using String.join()
+        lines[0] = String.join(" ", "  *  ", "  *  ", "** ", " *** ");
+        lines[1] = String.join(" ", " *  *", " *  *", "*   *", "**     ");
+        lines[2] = String.join(" ", "*    *", "*    *", "*   *", "**     ");
+        lines[3] = String.join(" ", "*    *", "*    *", "** ", " *** ");
+        lines[4] = String.join(" ", "*    *", "*    *", "*     ", "     *");
+        lines[5] = String.join(" ", " *  *", " *  *", "*     ", "     *");
+        lines[6] = String.join(" ", "  *  ", "  *  ", "*     ", " ** ");
+
+        // Use for-each loop to print each line
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }

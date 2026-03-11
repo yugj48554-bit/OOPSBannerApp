93b9ca819697bc3339b54b5c2220032e9fab09b3
yugj48554-bit <yugj48554@gmail.com>
2026-03-11 14:18:05 +0530

    UC6

--- oopsbannerapp.java
+++ oopsbannerapp.java
@@ -1,18 +1,57 @@
 public class oopsbannerapp{
-     public static void main(String[] args) {
-
-        String[] lines = {
-            String.join(" ", " *** ", " *** ", " **** ", " **** "),
-            String.join(" ", "*   *", "*   *", "*   *", "*     "),
-            String.join(" ", "*   *", "*   *", "*   *", "*     "),
-            String.join(" ", "*   *", "*   *", " **** ", " ***  "),
-            String.join(" ", "*   *", "*   *", "*     ", "    * "),
-            String.join(" ", "*   *", "*   *", "*     ", "    * "),
-            String.join(" ", " *** ", " *** ", "*     ", " **** ")
+     public static String[] getOPattern() {
+        return new String[] {
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
         };
+    }
 
-        // Enhanced for-loop to print each banner line
-        for (String line : lines) {
-            System.out.println(line);}
-	}
+
+    public static String[] getPPattern() {
+        return new String[] {
+            "**** ",
+            "*   *",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    ",
+            "*    "
+        };
+    }
+
+
+    public static String[] getSPattern() {
+        return new String[] {
+            " ****",
+            "*    ",
+            "*    ",
+            " *** ",
+            "    *",
+            "    *",
+            "**** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                oPattern[i] + " " +
+                oPattern[i] + " " +
+                pPattern[i] + " " +
+                sPattern[i]
+            );
+        }
+    }
 }
